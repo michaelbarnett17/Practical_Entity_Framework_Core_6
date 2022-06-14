e78f7ed18650a35e7a77c07ff83c9ea5a8e5060c
Michael Barnett <michaelbarnett17@gmail.com>
2022-06-13 23:19:28 -0500

    added more to chapter 3

---
+++ Ch_03_Code_First_From_Nothing/Activity_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using EFCore_DBLibrary;
+
+namespace Activity_2;
+
+public class Program
+{
+    private static IConfigurationRoot _configuration;
+    private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+
+    static void Main(string[] args)
+    {
+        BuildOptions();
+    }
+
+    static void BuildOptions()
+    {
+        _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+        _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+        _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+    }
+
+}
