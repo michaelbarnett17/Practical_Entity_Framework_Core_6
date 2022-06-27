803d07ba195f47e5deb7e48925767520bdd7d67d
Michael Barnett <michaelbarnett17@gmail.com>
2022-06-26 22:34:22 -0500

    Added Scaffolder to Chapter 2

---
+++ Ch_02_DB_Scaffolding/Scaffolder/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_Activity0201;
+
+class Program
+{
+    private static IConfigurationRoot _configuration;
+
+    static void Main(string[] args)
+    {
+        BuildConfiguration();
+        Console.WriteLine("Configuration String: " + _configuration.GetConnectionString("AdventureWorks2019"));
+    }
+
+    static void BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        _configuration = builder.Build();
+    }
+}
