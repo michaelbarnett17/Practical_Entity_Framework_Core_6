cbda4d5622d37bc2ea4a6ac92dd546e6a273f55e
Michael Barnett <michaelbarnett17@gmail.com>
2022-07-06 12:11:12 -0500

    completed chapter 6

---
+++ Ch_06_Data_Access_CRUD/InventoryModels/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryModels;
+
+public class Category : FullAuditModel
+{
+    [StringLength(InventoryModelsConstants.MAX_NAME_LENGTH)]
+    [Required]
+    public string Name { get; set; }
+
+    public CategoryDetail? Detail { get; set; }
+
+
+}
