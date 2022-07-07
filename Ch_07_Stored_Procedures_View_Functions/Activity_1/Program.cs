c7fd267d53712c9ac7a122ba3b0029cd4e39f816
Michael Barnett <michaelbarnett17@gmail.com>
2022-07-06 20:21:28 -0500

    completed chapter 7, activity 1

---
+++ Ch_07_Stored_Procedures_View_Functions/Activity_1/Program.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using EFCore_DBLibrary;
+using InventoryModels;
+
+namespace Activity_2;
+
+public class Program
+{
+    private static IConfigurationRoot _configuration;
+    private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+    private const string _loggedInUserId = "e2eb8989-a81a-4151-8e86-eb95a7961da2";
+
+
+    static void Main(string[] args)
+    {
+        BuildOptions();
+        DeleteAllItems();
+        EnsureItems();
+        UpdateItems();
+        ListInventory();
+        GetItemsForListing();
+    }
+
+    static void BuildOptions()
+    {
+        _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+        _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+        _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+    }
+
+    static void EnsureItems()
+    {
+        EnsureItem("Batman Begins", "You either die the hero or live long enough to see yourself become the villain", "Christian Bale, Katie Holmes");
+        EnsureItem("Inception", "You mustn't be afraid to dream a little bigger, darling", "Leonardo DiCaprio, Tom Hardy, Joseph Gordon-Levitt");
+        EnsureItem("Remember the Titans", "Left Side, Strong Side", "Denzell Washington, Will Patton");
+        EnsureItem("Star Wars: The Empire Strikes Back", "He will join us or die, master", "Harrison Ford, Carrie Fisher, Mark Hamill");
+        EnsureItem("Top Gun", "I feel the need, the need for speed!", "Tom Cruise, Anthony Edwards, Val Kilmer");
+    }
+
+        private static void EnsureItem(string name, string description, string notes)
+    {
+        Random r = new Random();
+        using (var db = new InventoryDbContext(_optionsBuilder.Options))
+        {
+            //determine if item exists:
+            var existingItem = db.Items.FirstOrDefault(x => x.Name.ToLower()
+                                                        == name.ToLower());
+
+            if (existingItem == null)
+            {
+                //doesn't exist, add it.
+                var item = new Item() 
+                { 
+                    Name = name,
+                    CreatedByUserId = _loggedInUserId,
+                    IsActive = true,
+                    Quantity = r.Next(1, 1000),
+                    Description = description,
+                    Notes = notes
+                };
+
+                db.Items.Add(item);
+                db.SaveChanges();
+            }
+        }
+    }
+
+    private static void ListInventory()
+    {
+        using (var db = new InventoryDbContext(_optionsBuilder.Options))
+        {
+            var items = db.Items.OrderBy(x => x.Name).ToList();
+            items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+        }
+    }
+
+    private static void GetItemsForListing()
+    {
+        using (var db = new InventoryDbContext(_optionsBuilder.Options))
+        {
+            var results = db.ItemsForListing.FromSqlRaw("EXECUTE dbo.GetItemsForListing").ToList();
+            foreach (var item in results)
+            {
+                var output = $"ITEM {item.Name}] {item.Description}";
+                if (!string.IsNullOrWhiteSpace(item.CategoryName))
+                {
+                    output = $"{output} has category: {item.CategoryName}";
+                }
+                Console.WriteLine(output);
+            }
+        }
+    }
+
+    private static void DeleteAllItems()
+    {
+        using (var db = new InventoryDbContext(_optionsBuilder.Options))
+        {
+            var items = db.Items.ToList();
+            db.Items.RemoveRange(items);
+            db.SaveChanges();
+        }
+    }
+
+    private static void UpdateItems()
+    {
+        using (var db = new InventoryDbContext(_optionsBuilder.Options))
+        {
+            var items = db.Items.ToList();
+            foreach (var item in items)
+            {
+                item.CurrentOrFinalPrice = 9.99M;
+            }
+            db.Items.UpdateRange(items);
+            db.SaveChanges();
+        }
+    }
+
+
+
+}
---
+++ Ch_07_Stored_Procedures_View_Functions/EFCore_DBLibrary/InventoryDbContext.cs
@@ -0,0 +1,105 @@
+﻿using InventoryModels;
+using InventoryModels.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_DBLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+        private const string _systemUserId = "2fd28110-93d0-427d-9207-d55dbca680fa";
+
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryDetail> CategoryDetails { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<GetItemsForListingDTO> ItemsForListing { get; set; }
+
+
+
+        public InventoryDbContext() { }
+
+        public InventoryDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                                   .SetBasePath(Directory.GetCurrentDirectory())
+                                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                        .HasMany(x => x.Players)
+                        .WithMany(p => p.Items)
+                        .UsingEntity<Dictionary<string, object>>(
+                            "ItemPlayers",
+                            ip => ip.HasOne<Player>()
+                                    .WithMany()
+                                    .HasForeignKey("PlayerId")
+                                    .HasConstraintName("FK_ItemPlayer_Players_PlayerId")
+                                    .OnDelete(DeleteBehavior.Cascade),
+                            ip => ip.HasOne<Item>()
+                                    .WithMany()
+                                    .HasForeignKey("ItemId")
+                                    .HasConstraintName("FK_PlayerItem_Items_ItemId")
+                                    .OnDelete(DeleteBehavior.ClientCascade)
+                        );
+
+            modelBuilder.Entity<GetItemsForListingDTO>(x =>
+            {
+                x.HasNoKey();
+                x.ToView("ItemsForListing");
+            });
+
+        }
+
+        public override int SaveChanges()
+        {
+            var tracker = ChangeTracker;
+
+            foreach (var entry in tracker.Entries())
+            {
+                if (entry.Entity is FullAuditModel)
+                {
+                    var referenceEntity = entry.Entity as FullAuditModel;
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            referenceEntity.CreatedDate = DateTime.Now;
+                            if (string.IsNullOrWhiteSpace(referenceEntity.CreatedByUserId))
+                            {
+                                referenceEntity.CreatedByUserId = _systemUserId;
+                            }
+                            break;
+                        case EntityState.Deleted:
+                        case EntityState.Modified:
+                            referenceEntity.LastModifiedDate = DateTime.Now;
+                            if (string.IsNullOrWhiteSpace(referenceEntity.LastModifiedUserId))
+                            {
+                                referenceEntity.LastModifiedUserId = _systemUserId;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+
+    }
+}
